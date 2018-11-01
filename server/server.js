e8e893ed7b408d59dda07e5dd0b5fc5e519daa71
Jakub <jakub94@gmx.de>
2018-11-01 16:40:39 +0100

    Acces-Controll allow cross origin *

--- server/server.js
+++ server/server.js
@@ -16,6 +16,13 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+//TODO DO NOT ALLOW CROSS ORIGIN IN PRODUCTION
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post("/tasks", authenticate, (req, res) => {
 
   var task = new Task({
