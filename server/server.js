2e9261046f485a27640a7aae103346bc41ac8b07
Jakub <jakub94@gmx.de>
2018-11-02 19:45:02 +0100

    allow x-auth header access controll

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 //TODO DO NOT ALLOW CROSS ORIGIN IN PRODUCTION
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-auth, Content-Type, Accept");
   next();
 });
 
