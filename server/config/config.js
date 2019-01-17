c1e7ef9690911be95d0e3bfcefbf12605ba3010f
Jakub <jakub94@gmx.de>
2019-01-17 13:14:17 +0100

    email reset bugfixes 1

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,7 @@ var env = process.env.NODE_ENV || "development";
 console.log("env ***** ", env);
 
 if(env === "development" || env === "test"){
+  //config.json is not pushed to the repository to keep the information private
   var config = require("./config.json");
   var envConfig = config[env];
 
--- server/tools/EmailSender.js
+++ server/tools/EmailSender.js
@@ -20,7 +20,7 @@ var sendResetPasswordMail = function(email, resetcode, req){
       subject: 'Password Reset Requested',
       text: `Please click this link within the next hour to reset your password:
              ${url}
-             if you did not request a password change, please ignore this mail.`
+             if you did not request a password change, please ignore this mail.` 
   }
 
   transporter.sendMail(mailOptions, function (err, res) {
